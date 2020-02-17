fbd69be95e4481daad27d5a945c243a474375536
Bill Garrett <garrett@astralfrontier.org>
2020-02-17 04:33:21 -0800

    Add blurb

--- src/pages/gurps-mashup.js
+++ src/pages/gurps-mashup.js
@@ -10,6 +10,17 @@ const GurpsMashupPage = () => (
   <Layout>
     <SEO title="GURPS Mashup" />
     <GurpsMashupClient />
+
+    <hr />
+    <h3>What is this?</h3>
+    <p>
+      Are you running some kind of RPG campaign, but don't have any ideas?
+      Do you want some kind of gonzo, out-there premise?
+      Start clicking "Randomize" and see where the dice take you.
+      Add more books if you need more variety.
+    </p>
+
+    <p>Note: book covers are loaded dynamically from the SJ Games website. Please be kind.</p>
   </Layout>
 )
 
